a24f0142cc8791e582bc56c7b9db7b130cb8dcaf
Freddy Monroy <fredydeltoro@gmail.com>
2023-09-27 00:19:36 -0600

    improve title

--- src/app/transactions/page.js
+++ src/app/transactions/page.js
@@ -4,7 +4,7 @@ import Table from "@/components/Table";
 export default function page() {
   return (
     <>
-      <h3>Transactions</h3>
+      <h3 style={{ marginBottom: "25px", color: "#ed4036" }}>Transactions</h3>
       <Table limit={15} pagination={true} />
     </>
   );
