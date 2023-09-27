7432af6b9c36c4fae13f8274a2da3582a40d02c4
Freddy Monroy <fredydeltoro@gmail.com>
2023-09-26 20:44:54 -0600

    Make header part of the layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,5 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <header>
-        <h1>
-          <img
-            src="https://ammper.com/wp-content/uploads/2020/07/logo-ammper-energia-mexico.png"
-            alt=""
-          />
-        </h1>
-      </header>
-    </main>
-  );
+  return <main className={styles.main}></main>;
 }
