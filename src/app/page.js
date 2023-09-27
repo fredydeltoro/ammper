2b9d084e4c93fcfa2c2c30dd4ab6921e3bfc52a8
Freddy Monroy <fredydeltoro@gmail.com>
2023-09-26 22:55:39 -0600

    Card for table

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,17 @@
-import styles from "./page.module.css";
 import Table from "@/components/Table";
+import Card from "@/components/Card";
 
 export default function Home() {
   return (
     <>
       <div className="row">
         <div className="col-12">
-          <h3>Transactions</h3>
-          <Table />
-          <a href="/transactions">See Full</a>
+          <Card title="Transactions">
+            <Table />
+            <a className="btn btn-primary card-ctrl" href="/transactions">
+              See Full
+            </a>
+          </Card>
         </div>
       </div>
     </>
